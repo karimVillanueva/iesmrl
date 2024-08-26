1dbbc807d7f5614dec86dfa76534959ceb7ec0c7
karimVillanueva <45979912+karimVillanueva@users.noreply.github.com>
2024-08-25 23:57:57 -0600

    upload new video

--- app/page.js
+++ app/page.js
@@ -23,12 +23,15 @@ export default function Home() {
     >
        <FaWhatsapp color='white' className='w-7 h-7 md:w-10 md:h-10' />
     </a>
-    <header className="bg-[#bca61c] p-4 opacity-70">
-          <div className="container mx-auto flex justify-between items-center">           
-            <div className="container mx-auto flex justify-between items-center">
-              INSTITUTO DE ESTUDIO SUPERIORES EN MEDICINA REGENERATIVA Y LONGEVIDAD 
+    <header className="bg-[#bca61c] p-4 ">
+          <div className="container mx-auto flex justify-between items-center">
+            {/* Logo */}
+            <div className="text-white text-xl font-bold ">
+              <Link href="/">
+                <Image src={logo} alt="Logo" className="h-8" /> {/* Ajusta la altura según sea necesario */}
+              </Link>
             </div>
-            
+
             {/* Nav Links */}
             <nav className="hidden md:flex space-x-6">
               <Link href="/" className="text-white hover:text-blue-300">Nosotros</Link>
@@ -55,15 +58,11 @@ export default function Home() {
     </header>
 
     <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-    <BackgroundVideo src={adnVideo} >    
-      <div className="bg-black opacity-70 mix-blend-multiply font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
-      <Link href="/">
-        <Image src={logo} alt="Logo"  />
-          {/* Ajusta la altura según sea necesario */}
-      </Link>
-      </div> 
-      
-     </BackgroundVideo>
+      <BackgroundVideo src={adnVideo} >
+        <div className="bg-black opacity-100 mix-blend-multiply font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+        <Image src={logo} alt="Logo" />
+          </div> 
+      </BackgroundVideo>
     </div>
    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
