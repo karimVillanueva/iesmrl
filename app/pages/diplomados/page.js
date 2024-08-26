b86aead4de9f79655b267899e2f98f6eedc5eeea
karimVillanueva <45979912+karimVillanueva@users.noreply.github.com>
2024-08-26 17:54:31 -0600

    dark mode

--- app/pages/diplomados/page.js
+++ app/pages/diplomados/page.js
@@ -124,108 +124,109 @@ export default function Diplomados() {
    }, [isPaused, speed]); 
 
   return (
-    <div className='container'>
-    <header className="bg-[#bca61c] p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* Logo */}
-        <div className="text-white text-xl font-bold">
-          <Link href="/">
-            <Image src={logo} alt="Logo" className="h-8" /> {/* Ajusta la altura según sea necesario */}
-          </Link>
-        </div>
-  
-        {/* Nav Links */}
-        <nav className="hidden md:flex space-x-6">
-          <Link href="/" className="text-white hover:text-blue-300">Nosotros</Link>
-          <Link href="/services" className="text-white hover:text-blue-300">Diplomados</Link>
-          <Link href="/contact" className="text-white hover:text-blue-300">Contacto</Link>
-        </nav>
-  
-        {/* Hamburger Icon */}
-        <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-white focus:outline-none">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-          </button>
-        </div>
+    <div className="dark:bg-gray-800">
+  <header className="bg-[#bca61c] dark:bg-gray-900 p-4">
+    <div className="container mx-auto flex justify-between items-center">
+      {/* Logo */}
+      <div className="text-white text-xl font-bold">
+        <Link href="/">
+          <Image src={logo} alt="Logo" className="h-8" /> {/* Ajusta la altura según sea necesario */}
+        </Link>
       </div>
-  
-      {/* Mobile Menu */}
-      <div className={`md:hidden mt-4 ${isOpen ? 'block' : 'hidden'}`}>
-        <Link href="/" className="block text-white hover:text-blue-300 mb-2">Inicio</Link>
-        <Link href="/services" className="block text-white hover:text-blue-300 mb-2">Servicios</Link>
+
+      {/* Nav Links */}
+      <nav className="hidden md:flex space-x-6">
+        <Link href="/" className="text-white hover:text-blue-300 dark:hover:text-yellow-400">Nosotros</Link>
+        <Link href="/services" className="text-white hover:text-blue-300 dark:hover:text-yellow-400">Diplomados</Link>
+        <Link href="/contact" className="text-white hover:text-blue-300 dark:hover:text-yellow-400">Contacto</Link>
+      </nav>
+
+      {/* Hamburger Icon */}
+      <div className="md:hidden">
+        <button onClick={toggleMenu} className="text-white dark:text-gray-300 focus:outline-none">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+          </svg>
+        </button>
       </div>
-    </header>
-  
-    <div className="relative">
-      {/* Botón de desplazamiento a la izquierda */}
-      <button
-        className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white text-black rounded-full p-2 shadow-md z-10"
-        onClick={scrollLeft}
-      >
-        &#8249;
-      </button>
-  
-      {/* Carrusel de cursos */}
-      <div
-        ref={carouselRef}
-        className="flex overflow-x-scroll space-x-4 p-4 no-scrollbar"
-      >
-        {courses.map((course) => (
-          <div
-            key={course.id}
-            className="min-w-[250px] md:min-w-[300px] lg:min-w-[400px] bg-gray-300 shadow-md rounded-md cursor-pointer transform hover:scale-105 transition-transform duration-200"
-            onClick={() => openPopup(course)}
-          >
-            <Image
-              src={logo}
-              alt={course.title}
-              className="w-full h-32 md:h-48 object-cover rounded-t-md"
-              height={300}
-              width={300}
-            />
-            <div className="p-4">
-              <h3 className="text-lg font-bold text-gray-700">{course.title}</h3>
-              <p className="text-sm text-gray-700">{course.description}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-  
-      {/* Botón de desplazamiento a la derecha */}
-      <button
-        className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white text-black rounded-full p-2 shadow-md z-10"
-        onClick={scrollRight}
-      >
-        &#8250;
-      </button>
-  
-      {/* Popup del temario */}
-      {selectedCourse && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full relative">
-            <button
-              className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-              onClick={closePopup}
-            >
-              &times;
-            </button>
-            <h2 className="text-2xl font-bold mb-4 text-gray-700">{selectedCourse.title}</h2>
-            <h3 className="text-lg font-semibold text-gray-700">Temario:</h3>
-            <pre className="whitespace-pre-wrap text-sm text-gray-700">{selectedCourse.syllabus}</pre>
-          </div>
-        </div>
-      )}
     </div>
-  
-    {/* Texto cambiante */}
-    <div className="flex flex-col items-center justify-center bg-black-100 space-y-4">
-      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg p-6 rounded-lg shadow-lg max-w-md">
-        <p className="text-white text-lg font-medium">{text}</p>
+
+    {/* Mobile Menu */}
+    <div className={`md:hidden mt-4 ${isOpen ? 'block' : 'hidden'}`}>
+      <Link href="/" className="block text-white dark:text-gray-300 hover:text-blue-300 dark:hover:text-yellow-400 mb-2">Inicio</Link>
+      <Link href="/services" className="block text-white dark:text-gray-300 hover:text-blue-300 dark:hover:text-yellow-400 mb-2">Servicios</Link>
+    </div>
+  </header>
+
+  <div className="relative">
+    {/* Botón de desplazamiento a la izquierda */}
+    <button
+      className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white dark:bg-gray-700 text-black dark:text-white rounded-full p-2 shadow-md z-10"
+      onClick={scrollLeft}
+    >
+      &#8249;
+    </button>
+
+    {/* Carrusel de cursos */}
+    <div
+      ref={carouselRef}
+      className="flex overflow-x-scroll space-x-4 p-4 no-scrollbar"
+    >
+      {courses.map((course) => (
+        <div
+          key={course.id}
+          className="min-w-[250px] md:min-w-[300px] lg:min-w-[400px] bg-gray-300 dark:bg-gray-700 shadow-md rounded-md cursor-pointer transform hover:scale-105 transition-transform duration-200"
+          onClick={() => openPopup(course)}
+        >
+          <Image
+            src={logo}
+            alt={course.title}
+            className="w-full h-32 md:h-48 object-cover rounded-t-md"
+            height={300}
+            width={300}
+          />
+          <div className="p-4">
+            <h3 className="text-lg font-bold text-gray-700 dark:text-gray-200">{course.title}</h3>
+            <p className="text-sm text-gray-700 dark:text-gray-300">{course.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+
+    {/* Botón de desplazamiento a la derecha */}
+    <button
+      className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white dark:bg-gray-700 text-black dark:text-white rounded-full p-2 shadow-md z-10"
+      onClick={scrollRight}
+    >
+      &#8250;
+    </button>
+
+    {/* Popup del temario */}
+    {selectedCourse && (
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 dark:bg-opacity-70 z-50">
+        <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-md w-full relative">
+          <button
+            className="absolute top-2 right-2 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+            onClick={closePopup}
+          >
+            &times;
+          </button>
+          <h2 className="text-2xl font-bold mb-4 text-gray-700 dark:text-white">{selectedCourse.title}</h2>
+          <h3 className="text-lg font-semibold text-gray-700 dark:text-gray-200">Temario:</h3>
+          <pre className="whitespace-pre-wrap text-sm text-gray-700 dark:text-gray-300">{selectedCourse.syllabus}</pre>
+        </div>
       </div>
+    )}
+  </div>
+
+  {/* Texto cambiante */}
+  <div className="flex flex-col items-center justify-center bg-black-100 dark:bg-gray-900 space-y-4">
+    <div className="bg-white dark:bg-gray-800 bg-opacity-20 dark:bg-opacity-40 backdrop-filter backdrop-blur-lg p-6 rounded-lg shadow-lg max-w-md">
+      <p className="text-white dark:text-gray-300 text-lg font-medium">{text}</p>
     </div>
   </div>
+</div>
+
   
   );
 };
