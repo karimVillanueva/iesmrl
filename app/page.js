d00da92ce3270ae89d8856277930ee1a6c64b21a
karimVillanueva <45979912+karimVillanueva@users.noreply.github.com>
2024-08-26 07:09:35 -0600

    chace title bg

--- app/page.js
+++ app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
 
     <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
       <BackgroundVideo src={adnVideo} >
-        <div className="bg-black opacity-100 mix-blend-multiply font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+        <div className="bg-white opacity-100 mix-blend-multiply font-bold text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
         <Image src={logo} alt="Logo" />
           </div> 
       </BackgroundVideo>
